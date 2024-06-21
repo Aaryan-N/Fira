39554c955949031b9867ede2910ac888eb82add2
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-06-21 20:56:53 +1000

    Discord partials and intents declared

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-const Discord = require("discord.js");
+const Discord= require("discord.js");
 
+const Client = new Discord.Client({
+        intents: [
+            Discord.GatewayIntentBits.GuildMessages,
+            Discord.GatewayIntentBits.GuildMembers,
+            Discord.GatewayIntentBits.DirectMessages,
+            Discord.GatewayIntentBits.MessageContent,
+            Discord.GatewayIntentBits.Guilds
+        ], partials: [
+            Discord.Partials.Message,
+            Discord.Partials.Channel,
+            Discord.Partials.GuildMember,
+            Discord.Partials.User,
+            Discord.Partials.GuildScheduledEvent
+        ]
+});
