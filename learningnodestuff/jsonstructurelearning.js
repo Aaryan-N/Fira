7af1208a136bb991b133e969fb8710e4c6add824
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-06-21 19:11:43 +1000

    Learning how to call things inside of a json object

--- learningnodestuff/jsonstructurelearning.js
+++ learningnodestuff/jsonstructurelearning.js
@@ -3,3 +3,15 @@ let user = {
     age: 16,
     hobbies: ["computers","java","js"]
 }
+
+let members = [{
+    id: 245342,
+    joinedTimeStamp: 342523462345
+}, {
+    id: 24534352,
+    joinedTimeStamp: 34252346232342345
+}
+
+]
+
+console.log(user.name);
