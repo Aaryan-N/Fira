432364d4878165cda89b53ea90004ba1e5e69443
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-06-23 08:28:00 +1000

    Commands have been fixed

--- scythe.js
+++ scythe.js
@@ -22,26 +22,21 @@ const client = new Client({
 });
 
 client.commands = new Collection();
-
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.name, command);
-    } else {
-      console.log(
-        `WARNING - The command at ${filePath} is missing a data or execute property`,
-      );
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
     }
-  }
 }
 
 const eventsPath = path.join(__dirname, 'events');
