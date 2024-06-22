22f5f2dab6356a17d2af6491e2f4ce138eb2bdb7
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-06-23 07:54:16 +1000

    Added functionality to register commands dynamically

--- core/registercommands.js
+++ core/registercommands.js
@@ -1,18 +1,14 @@
-import { REST, Routes } from "discord.js";
-import path from "node:path";
-import fs from "node:fs";
+const { REST, Routes } = require("discord.js");
+const path = require("node:path");
+const fs = require("node:fs");
 require('dotenv').config()
 
-dotenv.config();
-
-const rest = new REST({ version: "10" }).setToken(process.env.token);
-
 const commands = [];
-const foldersPath = path.join(__dirname, "commands");
+const foldersPath = path.join(__dirname, "../commands");
 const commandFolders= fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-  const commandsPath = path.join(commandPath, folder);
+  const commandsPath = path.join(foldersPath, folder);
   const commandFiles = fs
       .readdirSync(commandsPath)
       .filter((file) => file.endsWith(".js"));
@@ -20,25 +16,24 @@ for (const folder of commandFolders) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
     if ("data" in command && "execute" in command) {
-      client.commands.set(command.name, command);
+      commands.push(command.data.toJSON());
     } else {
-      console.log(
-          `WARNING - The command at ${filePath} is missing a data or execute property`,
-      );
+      console.log(`WARNING - The command at ${filePath} is missing a data or execute property`);
     }
   }
 }
 
+const rest = new REST().setToken(process.env.token);
 
-
-try {
+(async ()=> {
+  try {
   console.log("Started refreshing application (/) commands.");
 
-  await rest.put(Routes.applicationCommands(process.env.clientID), {
-    body: helpCommand,
-  });
+  const data = await rest.put(Routes.applicationCommands(process.env.clientID), {
+    body: commands});
 
   console.log("Successfully reloaded application (/) commands.");
 } catch (error) {
   console.error(error);
 }
+})();
