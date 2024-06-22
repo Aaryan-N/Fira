eb598836aae6145f6f05dea09ea625799aad4d5a
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-06-22 21:33:54 +1000

    Altered the way commands are registered and changed the type to ES modules

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const Discord = require("discord.js");
-const { token } = require("./token.json");
-const pollFunctions = require("./pollFunctions.js");
+import { token } from "./token.json";
+import { REST, Routes } from "discord.js";
 
 const Client = new Discord.Client({
   intents: [
@@ -20,31 +19,14 @@ const Client = new Discord.Client({
   ],
 });
 
-Client.on("ready", (client) => {
-  console.log(
-    "The bot is ready and online with the name of " + client.user.tag,
-  );
-});
+const rest = new REST({ version: "10" }).setToken(token);
 
-Client.on("messageCreate", (message) => {
-  if (message.author.bot === false) {
-    const inputToLowerCase = message.content.toLowerCase();
+try {
+  console.log("Started refreshing application (/) commands.");
 
-    if (inputToLowerCase == "!help" || inputToLowerCase == "!commands") {
-      message.reply(
-        "The !polls command displays all the polls that are currently active. \n" +
-          "The !closedpolls command displays all polls that are closed.\n" +
-          "The !poll <number> command displays a specific poll and the description.\n" +
-          "The !vote <number> yes/no give you the ability to vote on a specific poll.\n" +
-          "The !close <number> closes a poll that you ahve created .\n" +
-          "The ! create <question> creates a poll that user's can vote yes or no on",
-      );
-    } else if ("!create") {
-      let data = pollFunctions.createPoll();
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
 
-      message.reply(data);
-    }
-  }
-});
-
-Client.login(token);
+  console.log("Successfully reloaded application (/) commands.");
+} catch (error) {
+  console.error(error);
+}
