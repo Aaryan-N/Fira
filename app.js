91dbce58fe80557f3dd9938d80fe57aa8aa030e8
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-06-22 20:20:28 +1000

    Revert "Added an external js file command "
    This reverts commit c231759d121069b782a99c4dd8f07d6fe129b9c7.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const { token } = require("./token.json");
-const pollFunctions = require("./pollFunctions.js");
 
 const Client = new Discord.Client({
   intents: [
@@ -39,10 +38,6 @@ Client.on("messageCreate", (message) => {
           "The !close <number> closes a poll that you ahve created .\n" +
           "The ! create <question> creates a poll that user's can vote yes or no on",
       );
-    } else if ("!create") {
-      let data = pollFunctions.createPoll();
-
-      message.reply(data);
     }
   }
 });
--- pollFunctions.js
+++
@@ -1,5 +0,0 @@
-function createPoll() {
-  return "It works!";
-}
-
-module.exports.createPoll = createPoll;
