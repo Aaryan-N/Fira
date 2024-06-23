ec943ceb26b3540b3143085d8c078359059b5399
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-06-23 16:56:38 +1000

    Did a spot of debugging and the motd is printing properly albeit with excessive spacing

--- commands/minecraft/motd.js
+++ commands/minecraft/motd.js
@@ -7,14 +7,17 @@ module.exports = {
         .addStringOption(option => option.setName("mcaddress").setDescription("The address of the server (url or ip)").setRequired(true)),
     async execute(interaction) {
         await interaction.reply({ content:"Fetching..." });
+
         const serverAddress = interaction.options.getString('mcaddress');
+
         axios({
             method: 'get',
             url: `https://api.mcsrvstat.us/3/${serverAddress}`,
             responseType: 'json'
         })
-            .then(function (response) {
-                interaction.editReply(response.data.motd.clean);
+            .then(function (res) {
+                response = res.data.motd.clean.toString();
+                interaction.editReply(response);
             })
             .catch((err) => {
                 console.log(err)
