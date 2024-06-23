a6bd3db108de43435c62387e43a31c8326282ce3
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-06-23 11:54:32 +1000

    Far out this thing isn't even wanting to work

--- commands/fun/joke.js
+++ commands/fun/joke.js
@@ -1,20 +1,18 @@
 const { SlashCommandBuilder } = require("discord.js");
-const axios = require('axios');
+const axios = require("axios");
 
 let finalJoke = "";
+axios.get("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en").then((response) => {finalJoke = response.data}).catch(error => {
+    console.log(error);
+});
+
+
+
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("joke")
-        .setDescription("Replies with a lovely joke"),
-    async execute(interaction) {
-        axios
-            .get("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en")
-            .then((response) => {
-                finalJoke = response.data.text;
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        await interaction.reply(finalJoke);
-},
+  data: new SlashCommandBuilder()
+    .setName("joke")
+    .setDescription("Replies with a lovely joke"),
+  async execute(interaction) {
+      await interaction.reply("skbiid")
+  },
 };
--- testingAxios.js
+++ testingAxios.js
@@ -1,9 +1,13 @@
 const axios = require("axios");
-axios
-  .get("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en")
-  .then((response) => {
-    finalJoke = response.data.text;
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+
+let finalJoke = "";
+
+async function fetchJoke() {
+    try {
+        const { data }  = await axios.get("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en");
+        this.chart = data;
+        console.log(this.chart);
+    } catch (error) {
+        console.log(error)
+    }
+}
