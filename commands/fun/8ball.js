0c3e99fb6b9b2fafb8bb31d2680f42d54295fdbf
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-06-24 07:04:31 +1000

    Added 8Ball command, it doesn't quite function yet. Will fix it later.

---
+++ commands/fun/8ball.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder, EmbedBuilder} = require("discord.js");
+const axios = require("axios");
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("8ball")
+        .setDescription("Play a game of 8 ball!")
+        .addStringOption((option) =>
+            option
+                .setName("query")
+                .setDescription("Ask the 8 Ball what you want!")
+                .setRequired(true),
+        )
+        .addBooleanOption(option => option.setName('lucky').setDescription("Feeling lucky? Set this to true!")),
+
+    async execute(interaction) {
+        const userLuckyChoice = interaction.options.getBoolean("lucky");
+        const userBallQuery = interaction.options.getString("query");
+        const formattedUserBallQuery = userBallQuery.replace(/ /g, '+')
+        console.log(userLuckyChoice)
+        console.log(userBallQuery)
+        console.log(formattedUserBallQuery)
+
+        axios({
+            method: "get",
+            url: `https://eightballapi.com/api?question=${formattedUserBallQuery}&lucky=${userLuckyChoice}`,
+            responseType: "json",
+        })
+            .then(function (response) {
+                const ballEmbed = new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setTitle("8 Ball")
+                    .addFields(
+                        { name: "The 8 Ball's verdict!" , value: response.reading },
+                    )
+                    .setTimestamp()
+                    .setFooter({ text: "Sent using Scythe discord bot!" })
+                interaction.reply({embeds : [ballEmbed]});
+
+            })
+            .catch((err) => {
+                console.log(err);
+                interaction.reply("Did you type that in right?");
+            });
+    },
+};
