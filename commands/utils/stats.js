3b4092b8050111df2981bfeada4aea81bd482477
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-07-08 12:51:50 +1000

    Worked more on the stats command. Caching is a pain

--- commands/utils/stats.js
+++ commands/utils/stats.js
@@ -9,11 +9,14 @@ module.exports = {
         interaction.client.shard.fetchClientValues('guilds.cache.size')
             .then(results => {
                 const parsedGuildSize = results.reduce((prev, val) => prev + val, 0)
+                const owner = interaction.guild.ownerId;
+                const ownerDisplayName = interaction.client.users.cache.get(owner)
 
                 const statsEmbed = new EmbedBuilder()
                         .setColor(0x0099FF)
                         .addFields(
                             { name: "Amount of guilds the bot is currently in:", value: parsedGuildSize.toString() },
+                            { name: "Owner of server:", value: ownerDisplayName.username}
                         )
                         .setTimestamp()
                         .setFooter({ text: "Sent using Hydra" })
