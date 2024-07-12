05ea857e64131739bbbfdc2d2b8552db31b219bc
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-07-12 15:28:19 +1000

    Added some new things will probably remove though

--- fira.js
+++ fira.js
@@ -4,6 +4,7 @@ import { commandHandler } from './handlers/commandHandler.js';
 import { eventHandler } from './handlers/eventHandler.js';
 import chalk from 'chalk';
 import 'dotenv/config';
+import { CommandKit } from 'commandkit';
 
 export const client = new Client({
  intents: [
@@ -30,10 +31,16 @@ client.cluster = new ClusterClient(client);
 commandHandler(client).then(() => console.log(chalk.greenBright("Command handler is ready!")));
 eventHandler(client).then(() => console.log(chalk.greenBright("Event handler is ready!")));
 
+new CommandKit({
+ client,
+ devGuildIds: [process.env.GUILDID],
+ devUserIds: [process.env.NAVYUSERID],
+ skipBuiltInValidations: true,
+ bulkRegister: true,
+});
 
 try {
- client.login(process.env.TOKEN);
- console.log(chalk.greenBright('Successfully logged in!'));
+ client.login(process.env.TOKEN).then(() => {console.log(chalk.greenBright('Successfully logged in!'));})
 } catch (err) {
  console.log(chalk.redBright('Something went wrong with logging in. Here is the problem' + err));
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { ClusterManager } from 'discord-hybrid-sharding';
+import { ClusterManager, HeartbeatManager, ReClusterManager } from 'discord-hybrid-sharding';
 import 'dotenv/config';
 import chalk from 'chalk';
 
@@ -6,10 +6,24 @@ const __dirname = import.meta.dirname;
 
 const manager = new ClusterManager(`${__dirname}/fira.js`, {
  totalShards: 'auto',
+ totalClusters: 'auto',
  shardsPerClusters: 2,
  mode: 'process',
  token: process.env.TOKEN,
+ restarts: {
+  max: 4,
+  interval: 60000 * 60,
+ },
 });
 
 manager.on('clusterCreate', cluster => console.log(chalk.greenBright(`Launched Cluster ${cluster.id}`)));
-manager.spawn();
+
+manager.extend(
+ new HeartbeatManager({
+  interval: 3000,
+  maxMissedHeartbeats: 6,
+ })
+)
+
+manager.spawn().then(() => console.log(chalk.greenBright(`Cluster manager has been spawned!`))).catch((err) => {console.log(chalk.redBright(`Cluster manager died due to: ${err.message}`))})
+
