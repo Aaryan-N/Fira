be1610086c9a41bde8006fa220c7da8d2e95f2b5
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-07-12 13:50:53 +1000

    Done with the new arcade session command and templated the already active "error" message

--- commands/arcade/newArcadeSession.js
+++ commands/arcade/newArcadeSession.js
@@ -1,6 +1,9 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { alreadyActiveSession } from '../../templates/embeds/arcade/alreadyActiveSession.js';
 import axios from 'axios';
 import 'dotenv/config';
+import redBright from 'chalk';
+import { errorEmbed } from '../../templates/embeds/errors/errorEmbed.js';
 
 export default {
  data: new SlashCommandBuilder()
@@ -20,15 +23,29 @@ export default {
   const sessionDesc = interaction.options.getString('title');
   axios({
    method: 'post',
-   url: `https://api.mcsrvstat.us/3/${slackId}`,
+   url: `https://hackhour.hackclub.com/api/start/${slackId}`,
    headers: { Authorization: `Bearer ${process.env.HCBAPIKEY}` },
    data: {
     work : sessionDesc
    },
-   responseType: 'json',
   }).then(() => {
-   const succesful
-   interaction
+    const successfulStartArcade = new EmbedBuilder()
+     .setTitle("The arcade session has been started!")
+     .setColor(0x0099ff)
+     .setTimestamp()
+     .setFooter({
+      text: 'Sent using Fira!',
+      iconURL:
+       'https://cdn.discordapp.com/attachments/1171358299409617011/1260485101905645568/FiraLogo.jpeg?ex=668f7dba&is=668e2c3a&hm=7c023e2a9df44ca40816a976179870f3b55941196a431c537a5768a330690032&',
+     })
+    interaction.reply({ embeds: [successfulStartArcade] })
+  }).catch((err) => {
+   if (err.response.data.ok === false && err.response.data.error == "You already have an active session") {
+    interaction.reply({ embeds: [alreadyActiveSession]})
+   } else {
+    console.log(redBright(`Woah there has been an error with the new arcade session command. Here it is:` + err));
+    interaction.reply({ embeds: [errorEmbed] });
+   }
   })
  },
 };
